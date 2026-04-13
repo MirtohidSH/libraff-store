c90aa8b0592f33c7b49e3991a6248bd6119c2944
MirtohidSH <mirtohid444@gmail.com>
2026-04-13 11:24:19 +0400

    feat(entity): add Builder annotation in BookTransfer, StoreBookStock

--- src/main/java/org/example/libraffstore/entity/BookTransfer.java
+++ src/main/java/org/example/libraffstore/entity/BookTransfer.java
@@ -2,6 +2,7 @@ package org.example.libraffstore.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.libraffstore.enums.TransferStatus;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BookTransfer {
 
     @Id
