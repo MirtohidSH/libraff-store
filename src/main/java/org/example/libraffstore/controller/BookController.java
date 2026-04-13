f419fe9d59a024e59b59aa65c4e05204e1a14183
MirtohidSH <mirtohid444@gmail.com>
2026-04-13 11:29:05 +0400

    refactor(controller):  - Update BookController to return List instead of BookListResponse wrapper - Remove PayrollController (handled by @Scheduled in PayrollService)

--- src/main/java/org/example/libraffstore/controller/BookController.java
+++ src/main/java/org/example/libraffstore/controller/BookController.java
@@ -1,11 +1,14 @@
 package org.example.libraffstore.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.libraffstore.dto.response.BookListResponse;
+import org.example.libraffstore.dto.response.BookSingleResponse;
 import org.example.libraffstore.service.BookService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -16,7 +19,7 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public BookListResponse getEmployees() {
-        return bookService.findAll();
+    public ResponseEntity<List<BookSingleResponse>> getAllBooks() {
+        return ResponseEntity.ok(bookService.findAll());
     }
 }
